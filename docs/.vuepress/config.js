6e3f53f5f5b8a13d03bcd7e7c94b0b21cf7ceeae
SmokeyDays <fyq61002427@126.com>
2022-08-21 09:23:39 +0800

    docs: refactored the structure and added some text.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,25 @@
 module.exports = {
+  host: 'localhost',
   title: '无定牌',
-  description: '无定牌的相关信息'
+  description: '无定牌的相关信息',
+  themeConfig: {
+    sidebar: [
+      {
+        title: '欢迎来到走修至上主义的无定牌',
+        path: '/',
+      },
+      {
+        title: '规则',
+        path: '/rules/introduce',
+        collapsable: false,
+        children: [
+          { title: '简介', path: '/rules/introduce' },
+          { title: '卡牌', path: '/rules/cards' },
+          { title: '流程', path: '/rules/process' },
+          { title: '术语', path: '/rules/terms' },
+        ]
+      }
+    ],
+    subSidebar: 'auto'
+  },
 }
